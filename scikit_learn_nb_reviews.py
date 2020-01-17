bf3cdb07b55b56de16bf8fc3a34efff6ff83e977
Jodie Burchell <jodie.burchell@gmail.com>
2020-01-17 08:26:59 +0100

    [New] Added scripts and notebooks from Sagemaker

---
+++ scikit_learn_nb_reviews.py
@@ -0,0 +1,46 @@
+import os
+import pandas as pd
+import numpy as np
+
+from sklearn.externals import joblib
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+if __name__ == "__main__":
+    train_data_dir = os.environ['SM_CHANNEL_TRAIN']
+    model_output = os.environ['SM_MODEL_DIR']
+    ouptut_dir = os.environ['SM_OUTPUT_DATA_DIR']
+    
+    # Read in training data
+    train_data = os.path.join(train_data_dir, 'train_reviews.csv')
+    full_data = os.path.join(train_data_dir, 'full_reviews.csv')
+    if not os.path.exists(train_data):
+        raise ValueError(f'File {train_data} is not there')
+        
+    train = pd.read_csv(train_data, header=0)
+    full = pd.read_csv(full_data, header=0)
+    
+    # Apply transformations to features and target
+    # Features: convert into sparse matrix with Tf-Idf weights
+    train_x = train["review_body"].apply(lambda x: np.str_(x))
+    
+    tfidf_vect = TfidfVectorizer(max_features=5000)
+    tfidf_vect.fit(full["review_body"].apply(lambda x: np.str_(x)))
+    train_x_tfidf = tfidf_vect.transform(train_x)
+        
+    # Target: Encode as numeric
+    train_y = train["product_category"]
+    encoder = LabelEncoder()
+    train_y = encoder.fit_transform(train_y)
+    
+    nb_model = MultinomialNB()
+    nb_model = nb_model.fit(train_x_tfidf, train_y)
+    
+    joblib.dump(nb_model, os.path.join(model_output, "model.joblib"))
+    
+    
+def model_fn(model_output):
+    
+    nb_model = joblib.load(os.path.join(model_output, "model.joblib"))
+    return nb_model
